ced24616df3c6caa8bf49424f33e5edf03693608
CRONOS1993 <jona1993torres@gmail.com>
2023-06-10 06:57:51 -0500

    se crea la opcion Precio tambien se agrega un boton para guardar productos y se ingresa una tabla

--- index.py
+++ index.py
@@ -6,21 +6,35 @@ import sqlite3
 class Product:
 
     def __init__(self, window):
-        # Initializations 
+        # Iniciación  
         self.wind = window
         self.wind.title('MARKETMASTER Application')
 
-        #Creating a Frame Container
+        #Crar contenedor 
         frame = LabelFrame(self.wind, text = "Registrar nuevo producto")
         frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
         
-        #name input
+        # Nombre
         Label(frame, text = "Nombre: ").grid(row = 1, column = 0)
         self.name = Entry(frame)
+        self.name.focus()
         self.name.grid(row = 1, column = 1)
         
+        # Precio
+        Label(frame, text = "Precio: ").grid(row = 2, column = 0)
+        self.price = Entry(frame)
+        self.price.grid(row = 2, column = 1)
+        
+        #Boton Agregar Producto 
+        ttk.Button(frame, text = "Guardar Producto").grid (row = 3, columnspan = 2, sticky = W + E)
+        
+        #Tabla 
+        self.tree = ttk.Treeview(height  =  10, columns = 2)
+        self.tree.grid(row = 4, column = 0 , columnspan = 2)
+
 if __name__ == '__main__':
     window = Tk()
     application = Product(window)
     window.mainloop()
+       
        
