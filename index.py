6f2f2de235ecdd42c5277ed3f61b636b121472ed
CRONOS1993 <jona1993torres@gmail.com>
2023-06-10 06:29:53 -0500

    se crea un contenedor y se ingresa la opcion registrar y nombre

--- index.py
+++ index.py
@@ -10,7 +10,15 @@ class Product:
         self.wind = window
         self.wind.title('MARKETMASTER Application')
 
- 
+        #Creating a Frame Container
+        frame = LabelFrame(self.wind, text = "Registrar nuevo producto")
+        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+        
+        #name input
+        Label(frame, text = "Nombre: ").grid(row = 1, column = 0)
+        self.name = Entry(frame)
+        self.name.grid(row = 1, column = 1)
+        
 if __name__ == '__main__':
     window = Tk()
     application = Product(window)
